4a8d1f3ae5f099ca9135fd5d53080bf4a9dcb125
Furkan Elmas <furkanelmas321@gmail.com>
2022-05-14 18:21:50 +0300

    Ödev1.3
    Algoritma Soruları

---
+++ Ödev1.3/Program.cs
@@ -0,0 +1,54 @@
+﻿
+class MainClass
+{
+    static void Main()
+    {
+        int n = 0;
+
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Lütfen pozitif bir sayı girin(n): "); // Tam sayı olması gerektiğini kimse söylemedi :P
+                n = Convert.ToInt32(Math.Round(Convert.ToDouble(Console.ReadLine()), 0));
+
+                if (n > 0)
+                    break;
+                else
+                    Console.WriteLine("Negatif sayı giremezsiniz!");
+            }
+
+            catch
+            {
+                Console.WriteLine("Yanlış değer girdiniz!");
+            }
+        }
+
+        string[] words = new string[n];
+
+        while (true)
+        {
+            try
+            {
+
+                Console.WriteLine(n + " adet kelime girin");
+                words  = Console.ReadLine().Trim().Split(' ');
+
+                if (words.Length == n)
+                    break;
+                else if (words.Length < n)
+                    Console.WriteLine("Az kelime girdiniz!");
+                else if (words.Length > n)
+                    Console.WriteLine("Çok kelime girdiniz!");
+            }
+
+            catch
+            {
+                Console.WriteLine("Yanlış değer girdiniz!");
+            }
+        }
+        Array.Reverse(words);
+        string result = string.Join(" ", words);
+        Console.WriteLine(result);
+    }
+}
