16aedb50bcc087ef65c8d54a9872ef528e183f61
Furkan Elmas <furkanelmas321@gmail.com>
2022-06-15 13:20:19 +0300

    Ödev2.2 ve 2.3 yüklendi

---
+++ Ödev2.2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Max3Min3Numbers
+{
+    static void Main()
+    {
+        int[] numbers = new int[5];
+
+        Console.WriteLine("Toplam 20 adet pozitif tamsayı girin: ");
+
+        for (int i = 0; i < 5; i++)
+        {
+            try
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                while (input < 0)
+                {
+                    input = Convert.ToInt32(Console.ReadLine());
+                }
+                numbers[i] = input;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Yanlış karakter girdiniz. Numerik bir değer girin.");
+                i--;
+                continue;
+            }
+        }
+
+        Array.Sort(numbers);
+
+        TakeAvarage(numbers);
+    }
+    static void TakeAvarage(int[] numbers)
+    {
+        int[] maxNumbers = new int[3] {numbers[numbers.Length-1], numbers[numbers.Length - 2], numbers[numbers.Length - 3] };
+        int[] minNumbers = new int[3] {numbers[0], numbers[1], numbers[2]};
+
+        decimal maxNumbersAvarage = Convert.ToDecimal(maxNumbers.Sum()) / 3;
+        decimal minNumbersAvarage = Convert.ToDecimal(minNumbers.Sum()) / 3;
+
+        Console.WriteLine("\nEn büyük 3 sayının ortalaması: ");
+        Console.WriteLine(maxNumbersAvarage);
+        Console.WriteLine("\nEn küçük 3 sayının ortalaması: ");
+        Console.WriteLine(minNumbersAvarage);
+        Console.WriteLine("\nOrtalamalarının toplamları: ");
+        Console.WriteLine(maxNumbersAvarage + minNumbersAvarage);
+        Console.ReadLine();
+    }
+}
---
+++ Ödev2.3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VowelsInSentence
+{
+    static void Main()
+    {
+        char[] vowels = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+        int vowelCount = 0;
+
+        Console.WriteLine("Bir cümle girin: ");
+
+        string sentence = Console.ReadLine();
+        char[] vowelsInSentence = new char[sentence.Length];
+
+        foreach (char letter in sentence)
+        {
+            if (vowels.Contains(letter))
+            {
+                vowelsInSentence[vowelCount] = letter;
+                vowelCount++;
+            }
+        }
+
+        Array.Sort(vowelsInSentence);
+
+        Console.WriteLine("\nCümledeki sesli harfler: ");
+
+        foreach (char letter in vowelsInSentence)
+        {
+            Console.Write(letter);
+        }
+
+        Console.ReadLine();
+    }
+}
